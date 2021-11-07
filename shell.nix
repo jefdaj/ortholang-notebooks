f375994dfa2f2fc53900c26f0332ed96579f5dc8
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-11-07 10:53:06 -0800

    wip: add my jupyterWith fork

--- shell.nix
+++ shell.nix
@@ -1,15 +1,29 @@
+# Example of a Jupyter server with OrthoLang 0.9.5,
+# as well as Python + R with custom packages
+# Usage: nix-shell --command 'jupyter lab'
+
 let
   sources = import ./nix/sources.nix {};
+
   jupyter = import sources.jupyterWith {};
 
-  iPython = jupyter.kernels.iPythonWith {
-    name = "python";
+  python = jupyter.kernels.iPythonWith {
+    name = "shell.nix";
     packages = p: with p; [ numpy ];
   };
 
-  jupyterEnvironment = jupyter.jupyterlabWith {
-    kernels = [ iPython ];
+  r = jupyter.kernels.iRWith {
+    name = "shell.nix";
+    packages = p: with p; [ p.tidyverse ];
   };
 
+  # you can add a name, but there aren't packages like in python or R
+  ortholang = jupyter.kernels.ortholangKernel {
+    name = "pinned 0.9.5";
+  };
+
+  jupyterEnvironment = jupyter.jupyterlabWith {
+    kernels = [ python r ortholang ];
+  };
 in
   jupyterEnvironment.env
