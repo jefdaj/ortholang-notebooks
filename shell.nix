864b5c324e807c46cdfcd0eb9c7fa9a20f0164fb
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-11-08 09:50:46 -0800

    shell.nix: uncomment python, r kernels by default

--- shell.nix
+++ shell.nix
@@ -1,5 +1,4 @@
-# Example of a Jupyter server with OrthoLang 0.9.5,
-# as well as Python + R with custom packages
+# Example of a Jupyter server with OrthoLang 0.9.5
 # Usage: nix-shell --command 'jupyter lab'
 
 let
@@ -7,22 +6,26 @@ let
 
   jupyter = import sources.jupyterWith {};
 
+  ortholang = jupyter.kernels.ortholangKernel {};
+
+  # optional Python kernel. uncomment below to use it
   python = jupyter.kernels.iPythonWith {
     name = "shell.nix";
     packages = p: with p; [ numpy ];
   };
 
+  # optional R kernel. uncomment below to use it
   r = jupyter.kernels.iRWith {
     name = "shell.nix";
     packages = p: with p; [ p.tidyverse ];
   };
 
-  # you can optionally add your own name,
-  # but there aren't packages like in python or R
-  ortholang = jupyter.kernels.ortholangKernel {};
-
   jupyterEnvironment = jupyter.jupyterlabWith {
-    kernels = [ python r ortholang ];
+
+    # edit the kernels line to include the kernels you want:
+    # kernels = [ ortholang python r ortholang ];
+
+    kernels = [ ortholang ];
   };
 
 in
